c246661a3b0b784bb9a2a17d218b5ee24298bf70
Rakshita G Astagi <puti.astagi@gmail.com>
2026-06-13 19:02:26 +0530

    feat: add Login and Register pages with form inputs

--- jtfront/src/App.tsx
+++ jtfront/src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
