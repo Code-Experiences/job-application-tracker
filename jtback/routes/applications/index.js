366ed07e17b62639c35cec3fda2198867acbeca2
Shreyas Ashtamkar <shreyas.ashtamkar18@gmail.com>
2026-06-13 21:56:40 +0530

    feat: add CORS support and update dependencies

--- jtback/routes/applications/index.js
+++ jtback/routes/applications/index.js
@@ -1,14 +1,13 @@
 import { get_application, get_user_applications } from "./applications.js";
 
 export default async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    const { uid } = request.query;
-    return { list: await get_user_applications(fastify.prisma, uid) }
+  fastify.get('/:uid', async function (request, reply) {
+    const { uid } = request.params;
+    return { data: await get_user_applications(fastify.prisma, uid) }
   })
 
-  fastify.get('/:appid', async function (request, reply) {
-    const { uid } = request.query;
-    const { appid } = request.params;
+  fastify.get('/:uid/:appid', async function (request, reply) {
+    const { uid, appid } = request.params;
     return { data: await get_application(fastify.prisma, appid, uid) }
   })
 }
