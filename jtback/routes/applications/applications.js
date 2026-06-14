7b0f755c31a46b27b9097c2e630997107e98b39e
Shreyas Ashtamkar <shreyas.ashtamkar18@gmail.com>
2026-06-14 19:02:33 +0530

    feat: include company data in application details

--- jtback/routes/applications/applications.js
+++ jtback/routes/applications/applications.js
@@ -1,20 +1,26 @@
 export async function get_application(prisma, appid, uid) {
   return await prisma.Application.findUnique({
-    where: { application_id: appid, user_id: uid }
+    where: { 
+      application_id: appid, 
+      user_id: uid 
+    },
+    include: {
+      company: true
+    }
   });
 }
 
 export async function get_user_applications(prisma, uid) {
   return await prisma.Application.findMany({
+    where: {
+      user_id: uid
+    },
     select: {
       application_id: true,
       role: true,
       company: {
         select: { name: true }
       }
-    },
-    where: {
-      user_id: uid
     }
   })
 }
