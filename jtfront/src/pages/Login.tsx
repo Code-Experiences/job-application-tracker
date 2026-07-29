4ecbf8344745bf78a8f6503dcbb8be1ed2a586e5
Rakshita2 <puti.astagi@gmail.com>
2026-07-29 14:21:14 +0530

    feat: add global CSS import to Login and Register components

--- jtfront/src/pages/Login.tsx
+++ jtfront/src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import "../index.css";
 export default function Login() {
   const [loginInfo, setLoginInfo]=useState({
     email:'',
--- jtfront/src/pages/Register.tsx
+++ jtfront/src/pages/Register.tsx
@@ -1,3 +1,4 @@
+import "../index.css";
 export default function Register(){
     return (
          <div className="container">
